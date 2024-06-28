abfa243bc682629630d923beec6131df44a35cb4
Hollow-Serenity <dino.cosic1999@gmail.com>
2024-06-28 20:25:24 +0200

    validation setup and autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -91,3 +91,42 @@ interface Validatable {
   min?: number;
   max?: number;
 }
+
+// Validation function
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (validatableInput.minLength != null) {
+    isValid = isValid && validatableInput.value.toString().length >= validatableInput.minLength;
+  }
+  if (validatableInput.maxLength != null) {
+    isValid = isValid && validatableInput.value.toString().length <= validatableInput.maxLength;
+  }
+  if (validatableInput.min != null) {
+    isValid = isValid && typeof validatableInput.value === 'number' && validatableInput.value >= validatableInput.min;
+  }
+  if (validatableInput.max != null) {
+    if (typeof validatableInput.value === 'number') {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+    } else {
+      console.error('Invalid value type for max validation');
+      isValid = false;
+    }
+  }
+  return isValid;
+}
+
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
