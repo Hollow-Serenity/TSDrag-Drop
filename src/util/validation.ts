fe1aa885302a0a042650ce0657c216c54790dcbc
Hollow-Serenity <dino.cosic1999@gmail.com>
2024-06-29 23:14:46 +0200

    updated the validation to use a switch case instead of the many if statements.

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -10,34 +10,25 @@ export interface Validatable {
 
 export function validate(validatableInput: Validatable) {
   let isValid = true;
+
   if (validatableInput.required) {
     isValid = isValid && validatableInput.value.toString().trim().length !== 0;
   }
-  if (
-    validatableInput.minLength != null &&
-    typeof validatableInput.value === 'string'
-  ) {
-    isValid =
-      isValid && validatableInput.value.length >= validatableInput.minLength;
+
+  switch (true) {
+    case validatableInput.minLength != null && typeof validatableInput.value === 'string':
+      isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+      break;
+    case validatableInput.maxLength != null && typeof validatableInput.value === 'string':
+      isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+      break;
+    case validatableInput.min != null && typeof validatableInput.value === 'number':
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+      break;
+    case validatableInput.max != null && typeof validatableInput.value === 'number':
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+      break;
   }
-  if (
-    validatableInput.maxLength != null &&
-    typeof validatableInput.value === 'string'
-  ) {
-    isValid =
-      isValid && validatableInput.value.length <= validatableInput.maxLength;
-  }
-  if (
-    validatableInput.min != null &&
-    typeof validatableInput.value === 'number'
-  ) {
-    isValid = isValid && validatableInput.value >= validatableInput.min;
-  }
-  if (
-    validatableInput.max != null &&
-    typeof validatableInput.value === 'number'
-  ) {
-    isValid = isValid && validatableInput.value <= validatableInput.max;
-  }
+
   return isValid;
 }
